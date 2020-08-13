a1be67eab0050eb36f6abf4c8a8d6092b57525b9
GITCART <cartolin1011@gmail.com>
2020-08-13 18:02:52 -0500

    Agregar archivos de proyecto.

---
+++ SocialMedia.Infrastructure/Repositories/PostMongoRepository.cs
@@ -0,0 +1,29 @@
+﻿using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostMongoRepository : IPostRepository
+    {
+        public async Task<IEnumerable<Publicacion>> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Publicacion
+            {
+                /*PostId = x,
+                Description = $"Description{x}",
+                Date = DateTime.Now,
+                Image = $"https://images.com/{x}",
+                UserId = x * 2*/
+            });
+
+            await Task.Delay(10);
+
+            return posts;
+        }
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+
+        private readonly SocialMediaApiContext _socialMediaApiContext;
+
+        public PostRepository(SocialMediaApiContext socialMediaApiContext)
+        {
+            _socialMediaApiContext = socialMediaApiContext;
+        }
+
+        public async Task<IEnumerable<Publicacion>> GetPosts()
+        {
+            var posts = await _socialMediaApiContext.Publicacion.ToListAsync();
+
+            return posts;
+        }
+
+    }
+}
